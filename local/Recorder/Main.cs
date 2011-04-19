d9cfaa851e93c762cfee8549c1821e09eb45c710
Callum Rogers <callumrogers@hotmail.co.uk>
2011-04-19 01:38:29 +0100

    Seperated out process reading

--- local/Recorder/Main.cs
+++ local/Recorder/Main.cs
@@ -17,19 +17,23 @@ namespace Recorder
 		
 		public static IEnumerable<double> GetTemps()
 		{
-			var p = new Process();
-			p.StartInfo.FileName = "aticonfig";
-			p.StartInfo.Arguments = "--odgt --adapter=all";
-			p.StartInfo.UseShellExecute = false;
-			p.StartInfo.RedirectStandardOutput = true;
-			p.Start();
-			
-			string text = p.StandardOutput.ReadToEnd();
-			
+			var text = ReadProcess("aticonfig", "--odgt --adapter=all");			
 			var r = new Regex(@"Temperature - (?<temp>\d{1,3}\.\d{1,2})");
 			
 			foreach(Match m in r.Matches(text))
 				yield return double.Parse(m.Groups["temp"].Value);
+		}
+		
+		public static string ReadProcess(string program, string args)
+		{
+			var p = new Process();
+			p.StartInfo.FileName = program;
+			p.StartInfo.Arguments = args;
+			p.StartInfo.UseShellExecute = false;
+			p.StartInfo.RedirectStandardOutput = true;
+			p.Start();
+			
+			return p.StandardOutput.ReadToEnd();
 		}
 	}
 }
